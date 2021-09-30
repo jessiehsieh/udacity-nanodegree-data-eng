30f0c97a0c257230bb98efb4bf211e3693947dbd
root <jessie.hsiehpm@gmail.com>
2021-09-30 11:42:22 +0000

    fixed drop statements

--- project1/sql_queries.py
+++ project1/sql_queries.py
@@ -1,10 +1,10 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
@@ -62,20 +62,20 @@ time_table_create = (""" CREATE TABLE IF NOT EXISTS time (
 
 # INSERT RECORDS
 
-songplay_table_insert = (""" DROP TABLE IF EXISTS songplays
+songplay_table_insert = (""" 
 """)
 
-user_table_insert = (""" DROP TABLE IF EXISTS users
+user_table_insert = (""" 
 """)
 
-song_table_insert = (""" DROP TABLE IF EXISTS songs
+song_table_insert = (""" 
 """)
 
-artist_table_insert = (""" DROP TABLE IF EXISTS artists
+artist_table_insert = (""" 
 """)
 
 
-time_table_insert = (""" DROP TABLE IF EXISTS time
+time_table_insert = (""" 
 """)
 
 # FIND SONGS
