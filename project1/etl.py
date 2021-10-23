511c0a0716ce8226a33cd06fec545b2b896195bf
root <jessie.hsiehpm@gmail.com>
2021-10-23 19:09:15 +0000

    add song data

--- project1/etl.py
+++ project1/etl.py
@@ -74,7 +74,7 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (row.ts, row.userId, row.level, songid, artistid, sessionId, location, userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
