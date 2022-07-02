3b5b6819cc7cdfb93b40b9000308a168b33c47d1
root <jessie.hsiehpm@gmail.com>
2022-07-02 22:25:31 +0000

    add create function to this script too

--- project3/etl.py
+++ project3/etl.py
@@ -2,6 +2,8 @@ import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
+from create_tables import create_tables
+
 
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
@@ -22,7 +24,10 @@ def main():
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
     
-    #load_staging_tables(cur, conn)
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+    
+    load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
     conn.close()
